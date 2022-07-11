647d81e2c9b470d7d72939378aac0a6d6bd62a39
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2022-07-11 20:07:04 +0200

    Initial commit

---
+++ minhook.cpp
@@ -0,0 +1,78 @@
+#include <MinHook.h>
+
+extern "C" __declspec(dllexport)
+bool mh_initialize()
+{
+    return MH_Initialize() == MH_OK;
+}
+
+extern "C" __declspec(dllexport)
+bool mh_uninitialize()
+{
+    return MH_Uninitialize() == MH_OK;
+}
+
+extern "C" __declspec(dllexport)
+void* mh_create_hook(void* function, void* detour)
+{
+    void* original;
+    if (MH_CreateHook(function, detour, &original) != MH_OK)
+        return nullptr;
+
+    return original;
+}
+
+extern "C" __declspec(dllexport)
+bool mh_remove_hook(void* function)
+{
+    return MH_RemoveHook(function) == MH_OK;
+}
+
+extern "C" __declspec(dllexport)
+bool mh_enable_hook(void* function)
+{
+    return MH_EnableHook(function) == MH_OK;
+}
+
+extern "C" __declspec(dllexport)
+bool mh_disable_hook(void* function)
+{
+    return MH_DisableHook(function) == MH_OK;
+}
+
+struct CameraWorld;
+struct vec2;
+
+extern "C" __declspec(dllexport)
+void (__thiscall *real_GameScreenshake)(CameraWorld* camera, vec2* pos) = nullptr;
+
+extern "C" __declspec(dllexport)
+void GameScreenshake_hook_target(CameraWorld* camera, vec2* pos, float strength)
+{
+    __asm movd xmm1, strength;
+    real_GameScreenshake(camera, pos);
+}
+
+extern "C" __declspec(dllexport)
+void __thiscall GameScreenshake_xmm1_shim(CameraWorld* camera, vec2* pos)
+{
+    float strength;
+    __asm movd strength, xmm1
+
+    GameScreenshake_hook_target(camera, pos, strength);
+}
+
+
+BOOL WINAPI DllMain(
+    HINSTANCE hinstDLL,
+    DWORD fdwReason,
+    LPVOID lpReserved)
+{
+    switch(fdwReason) { 
+    case DLL_PROCESS_DETACH:
+        MH_Uninitialize();
+        break;
+    }
+
+    return TRUE;
+}
---
+++ minhook.lua
@@ -0,0 +1,74 @@
+function load_minhook(path)
+    local dll_path = path .. "/luajit_minhook.dll"
+    local ffi = require("ffi")
+    ffi.cdef([[
+    bool mh_initialize();
+    bool mh_uninitialize();
+    void* mh_create_hook(void* function, void* detour);
+    bool mh_remove_hook(void* function);
+    bool mh_enable_hook(void* function);
+    bool mh_disable_hook(void* function);
+
+    void (*real_GameScreenshake)(struct CameraWorld*, struct vec2*);
+    void GameScreenshake_hook_target(struct CameraWorld* camera, struct vec2* pos, float strength);
+    void __thiscall GameScreenshake_xmm1_shim(struct CameraWorld* camera, struct vec2* pos);
+    ]])
+
+    minhook = {}
+
+    lib = ffi.load(dll_path)
+    minhook.lib = lib
+
+    minhook.initialize = function()
+        return lib.mh_initialize()
+    end
+
+    minhook.uninitialize = function()
+        return lib.mh_initialize()
+    end
+
+    function is_function_pointer(typ)
+        return tostring(typ):find("(*)") ~= nil
+    end
+
+    function function_pointer_type(typ)
+        if is_function_pointer(typ) then
+            return typ
+        end
+
+        return ffi.typeof("$*", typ)
+    end
+
+    function minhook.create_hook(func, hook)
+        local func_type = function_pointer_type(ffi.typeof(func))
+        local hook_func = ffi.cast(func_type, hook)
+        local ret = lib.mh_create_hook(func, hook_func)
+
+        if ret == nil then
+            return nil
+        end
+
+        local original = ffi.cast(func_type, ret)
+        return {
+            func = func,
+            hook_func = hook_func,
+            original = original
+        }
+    end
+
+    function minhook.remove(func)
+        return lib.mh_remove_hook(func)
+    end
+
+    function minhook.enable(func)
+        return lib.mh_enable_hook(func)
+    end
+
+    function minhook.disable(func)
+        return lib.mh_disable_hook(func)
+    end
+
+    return minhook
+end
+
+return load_minhook
